7f095167c7933eb663c131ffc5ab2340226589d3
Aleksandra Cheidze <a.cxeidze@gmail.com>
2023-08-27 17:48:09 +0200

    add: method printMenu

---
+++ src/BudgetApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BudgetApp {
+ private Scanner scanner;
+
+
+
+
+
+ private void printMenu() {
+  System.out.println("1. Добавить расходы");
+  System.out.println("2. Посмотреть отчет расходов");
+  System.out.println("3. Посмотреть расходы по категориям");
+  System.out.println("4. Выйти");
+ }
+ }
+
+
+
